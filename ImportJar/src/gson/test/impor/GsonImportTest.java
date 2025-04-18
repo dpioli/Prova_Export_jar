e07feb230715c7d3c56a0edbd7054447915ee954
dpioli <diegopioli02@gmail.com>
2025-04-18 11:03:41 +0200

    modifiche per la gestione dei dati

--- ImportJar/src/gson/test/impor/GsonImportTest.java
+++ ImportJar/src/gson/test/impor/GsonImportTest.java
@@ -1,32 +1,29 @@
 package gson.test.impor;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 
-import com.google.gson.Gson;
 
 public class GsonImportTest {
 
 	public static void main(String[] args) {
-		Gson gson = new Gson();
+		Persistenza p = new Persistenza();
 		
-		ArrayList<Integer> numeri = new ArrayList<>();
+		ArrayList<Integer> numeri = Persistenza.caricaNumeri();
 		
-		for (int i = 0; i < 100; i++)
-			numeri.add(i);
+		boolean continua = false;
+		
+		while(!continua) {
+			int nuovoNum = InputDati.leggiIntero("Inserisci un numero ('0' per terminare) > ");
+			if(nuovoNum == 0) {
+				continua = true;
+			} else {
+				numeri.add(nuovoNum);
+			}
+		}
 		
 		System.out.println(numeri);
-		System.out.println(gson.toJson(numeri));
-		try {
-			FileWriter fileWriter = new FileWriter("../ImportJar/numeri/num.json");
-			gson.toJson(numeri, fileWriter);
-			System.out.println("JSON saved to num.json successfully!");
-			fileWriter.close();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		
+		Persistenza.salvanumeri(numeri);
 		
 
 	}
---
+++ ImportJar/src/gson/test/impor/Persistenza.java
@@ -0,0 +1,65 @@
+package gson.test.impor;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+public class Persistenza {
+	
+	private static final String FILE_DATI = "numeri/num.json";
+	private static Gson gson;
+	
+	public Persistenza() {
+		this.gson= new GsonBuilder().setPrettyPrinting().create();
+	}
+	
+	private static <T> void salva(T oggetto, String fpath) {
+		try(FileWriter wr = new FileWriter(fpath)){
+			gson.toJson(oggetto, wr);
+			wr.close();
+		} catch (IOException e) {
+			System.err.println("Errore durente il salvataggio: " + e.getMessage());
+		}
+	}
+	
+	private static <T> T carica(Type typeOf, String fpath) {
+		T oggetto = null;
+		File file = new File(fpath);
+		if(!file.exists()) {
+			System.err.println("File non trovato: " + fpath);
+			return null;
+		}
+		
+		try(FileReader rd = new FileReader(fpath)){
+			oggetto = gson.fromJson(rd, typeOf);
+		} catch(IOException e) {
+			System.err.println("Errore durante il caricamento: " + e.getMessage());
+		}
+		return oggetto != null ? oggetto : null;
+	}
+	
+	private static <T> T creaOggettoVuoto(Class<T> classe) {
+        try {
+            return classe.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+	
+	public static void salvanumeri(ArrayList<Integer> numeri) {
+		salva(numeri, FILE_DATI);
+	}
+	
+	public static ArrayList<Integer> caricaNumeri(){
+	    Type listType = new TypeToken<ArrayList<Integer>>() {}.getType();
+	    ArrayList<Integer> gerarchie = carica(listType, FILE_DATI);
+	    if(gerarchie == null) {
+	    	return new ArrayList<Integer>();
+	    }
+	    return gerarchie;
+	}
+}
